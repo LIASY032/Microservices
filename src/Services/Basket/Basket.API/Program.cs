b5a3a822a0096c5c58460ac532f1d3cfb5c61452
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-09-29 16:39:13 +0930

    set docker for basket.api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -18,6 +18,10 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+//builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+//                (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
